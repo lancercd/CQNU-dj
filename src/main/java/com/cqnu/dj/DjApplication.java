62c8d651709c537ed5da7832edac43f4465f57b6
lancercd <1311282756@qq.com>
2021-02-24 17:17:28 +0800

    src

---
+++ src/main/java/com/cqnu/dj/DjApplication.java
@@ -0,0 +1,13 @@
+package com.cqnu.dj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cqnu/dj/DjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cqnu.dj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
