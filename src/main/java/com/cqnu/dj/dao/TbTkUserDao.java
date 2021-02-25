87c965ce3e2bede77730465173d8e6d73be98a06
lancercd <1311282756@qq.com>
2021-02-25 13:12:03 +0800

    mvc配置   dao

---
+++ src/main/java/com/cqnu/dj/dao/TbTkUserDao.java
@@ -0,0 +1,19 @@
+package com.cqnu.dj.dao;
+
+import com.cqnu.dj.pojo.TbTkUser;
+
+import java.util.Collection;
+
+public class TbTkUserDao {
+
+    public Collection<TbTkUser> getAllUser(){
+
+        return null;
+    }
+
+    public Collection<TbTkUser> getUserById(int id){
+
+        return null;
+    }
+
+}
