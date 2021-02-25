13e83abe7b1d78ac601af2b1692980fbc137456b
lancercd <1311282756@qq.com>
2021-02-25 13:10:50 +0800

    user 表

--- src/main/java/com/cqnu/dj/pojo/TbTkUser.java
+++ src/main/java/com/cqnu/dj/pojo/TbTkUser.java
@@ -9,5 +9,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class TbTkUser {
     private int userId;
-    private String phone;
+    private String userPhone;
+    private String userCid;
+    private String userSid;
+    private String userName;
+    private String userPwd;
+    private int trainerId;
+    private int trainerTime;
+    private boolean isPass;
+    private boolean isAtt;
+    private boolean isAllin;
+    private boolean isCourse;
+    private int dshipId;
+    private int userSex;
+    private String userPic;
+    private int userMm;
 }
